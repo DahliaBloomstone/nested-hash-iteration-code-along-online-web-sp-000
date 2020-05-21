6e2178a0647b475f12818a4ed35218f712cd6d83
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-21 23:45:35 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,10 +17,6 @@ contacts = {
   })
 
 
-
-def remove_strawberry(contacts)
-
-
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
